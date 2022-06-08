7eea2b4e2c42d3719072982f8c7ce29949bfcd85
Kwzrthieery <kwzrthieery@gmail.com>
2022-06-08 08:21:56 -0700

    Added main.h

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,19 @@
+#ifndef _MAIN_
+#define _MAIN_
+#include <unistid.h>
+
+int _putchar(char);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int);
+int _isalpha(int);
+int _print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
+void times_table(void);
+int add(int, int);
+void print_to_98(int);
+void print_times_table(int);
+
+#endif /* MAIN */
