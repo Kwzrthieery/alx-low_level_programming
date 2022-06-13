0d10c6e6b70d8135ba9bab14a68217d3969a2f1d
Kwzrthieery <kwzrthieery@gmail.com>
2022-06-13 08:32:45 -0700

    Added 3 & 4

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - main function
+ *
+ * Return: End the program
+ */
+int main(void)
+{
+	char l = 97;
+
+	while(l <= 122)
+	{
+		putchar(1);
+		++l;
+	}
+	putchar(10);
+
+	return (0);
+}
+
---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(void)
+{
+	char low = 97;
+	char up = 65;
+
+	while (low <= 122)
+	{
+		putchar(low);
+		++low;
+	}
+	while (up <= 98)
+	{
+		putchar(up);
+		++up;
+	}
+
+	putchar(10);
+
+	return (0);
+}
