75b0dd276d79d72d6098ac87e2be47f6bb17bc34
Kwzrthieery <kwzrthieery@gmail.com>
2022-06-16 09:18:37 -0700

    Solved 2

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -14,13 +14,11 @@ int main(void)
 	i = 0;
 
 	/*
-         * while (i < 10)
-	 * { 
+	 * while (i < 10)
+	 * {
 	 * putchar(i);
-	 * } 
-         */
-
-	printf("Infinite loop avoided! \\o/\n");
-
+	 * }
+	 */
+	printf("Infinite loop avoided! \\0/\n");
 	return (0);
 }
