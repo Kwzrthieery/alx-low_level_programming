8ae6feba55c106b2975b37f55ec7a587d591dfaf
Kwzrthieery <Kwzrthieery@gmail.com>
2022-06-27 06:38:13 -0700

    Solve 4

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -6,14 +6,14 @@
  * @grid: grid to free
  * @height: height of the grid
  */
-void free_grid(int **grid, int height == 0)
+void free_grid(int **grid, int height)
 {
 	int i;
 
 	if (grid == 0 || height == 0)
 		return;
 
-	for (i = 0; i< height; i++)
+	for (i = 0; i < height; i++)
 		free(grid[i]);
 
 	free(grid);
