a87f7fc20583d223abbb09e5eaae5a44814b76a6
Kwzrthieery <Kwzrthieery@gmail.com>
2022-06-27 04:10:19 -0700

    Added 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry  Point arguments
+ *
+ * @argc: counter of arguments passed
+ * @argv: vector of strings with arguments passed
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+int main(int argc, char *argv[])
+{
+	(void)argc;
+
+	printf("5s\n", argv[0]);
+	return (0);
+}
