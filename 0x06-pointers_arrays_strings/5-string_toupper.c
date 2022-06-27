6918f0fbeaf1bbd614f0564ed76c03dc283c3448
Kwzrthieery <Kwzrthieery@gmail.com>
2022-06-27 08:08:09 -0700

    Add 0x06

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * string_toupper - change all owercas letters of a string
+ * to uppercase
+ *
+ * @S: string to modify
+ *
+ * Return: the resulting string
+ */
+char *string_toupper(char *s)
+{
+
+	int x = 0;
+
+	while (*(s + x))
+	{
+		if ((*(s + x) >= 97) && (*(s = x) <= 122))
+			*(s + x) = *(s + x) - 32;
+		x++;
+	}
+	return (s);
+}
