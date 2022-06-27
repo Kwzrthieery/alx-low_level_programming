4b82e50ee6e5aa56b337ceacce8822d0a8a8311c
Kwzrthieery <Kwzrthieery@gmail.com>
2022-06-27 08:28:42 -0700

    Add 0x06

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -14,7 +14,7 @@ char *string_toupper(char *s)
 
 	while (*(s + x))
 	{
-		if ((*(s + x) >= 97) && (*(s = x) <= 122))
+		if ((*(s + x) >= 97) && (*(s + x) <= 122))
 			*(s + x) = *(s + x) - 32;
 		x++;
 	}
